bfe53226d2bd1382f3e1c8b92fdd1db300d5ebf1
Aryan Patil <84935470+aryanator@users.noreply.github.com>
2024-10-30 17:19:32 -0400

    Update utils.py
    Added another method to calculate centroids where we don't let the thin sections in the object affect the accuracy of centroid.

--- supervision/geometry/utils.py
+++ supervision/geometry/utils.py
@@ -1,6 +1,10 @@
 import numpy as np
-
 from supervision.geometry.core import Point
+from scipy.ndimage import binary_erosion, binary_dilation
+import numpy as np
+import torch
+from PIL import Image, ImageDraw
+from scipy.ndimage import binary_fill_holes
 
 
 def get_polygon_center(polygon: np.ndarray) -> Point:
@@ -45,3 +49,57 @@ def get_polygon_center(polygon: np.ndarray) -> Point:
     center = np.average(centroids, axis=0, weights=signed_areas).round()
 
     return Point(x=center[0], y=center[1])
+
+
+def extract_centroids_from_segmentations(polygons, img_height, img_width, bounding_boxes):
+    """Calculate centroids from COCO polygon annotations.
+
+    Args:
+        polygons: List of COCO polygon annotations representing object shapes.
+        img_height: Height of the image to create the mask.
+        img_width: Width of the image to create the mask.
+        bounding_boxes: List of bounding boxes for each polygon.
+
+    Returns:
+        A PyTorch tensor containing the calculated centroids of the objects.
+    """
+    masks_list = []
+    estimated_centroids = []
+    
+    for poly in polygons:
+        # Create a blank mask image
+        mask_image = Image.new('L', (img_width, img_height), 0)
+        ImageDraw.Draw(mask_image).polygon(poly[0], outline=1, fill=1)
+        mask_array = np.array(mask_image)
+        masks_list.append(mask_array)
+
+        # Calculate initial centroid
+        y_coords, x_coords = np.where(mask_array == 1)
+        centroid_estimate = [np.mean(x_coords), np.mean(y_coords)]
+        estimated_centroids.append(centroid_estimate)
+
+    refined_masks = []
+    for idx, mask in enumerate(masks_list):
+        height_diff = abs(bounding_boxes[idx][0] - bounding_boxes[idx][2]).item()
+        width_diff = abs(bounding_boxes[idx][1] - bounding_boxes[idx][3]).item()
+        min_threshold = int(min(height_diff, width_diff) / 8)
+
+        # Erode and then dilate the mask
+        refined_mask = binary_dilation(binary_erosion(mask, iterations=min_threshold), iterations=min_threshold)
+        refined_masks.append(refined_mask.astype(int))
+
+    centroid_positions = []
+    for idx, refined_mask in enumerate(refined_masks):
+        if np.sum(refined_mask) == 0:
+            centroid = estimated_centroids[idx]
+        else:
+            y_coords, x_coords = np.where(refined_mask == 1)
+            if x_coords.size == 0 or y_coords.size == 0:
+                centroid = estimated_centroids[idx]
+            else:
+                centroid = [np.mean(x_coords), np.mean(y_coords)]
+        centroid_positions.append(centroid)
+
+    return torch.tensor(centroid_positions, dtype=torch.float)
+
+
